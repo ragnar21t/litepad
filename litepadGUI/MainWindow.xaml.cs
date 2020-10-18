db4ec8de8296feaa5e466707fbb29b3409e31a5e
ragnar21t <ragnar21t@gmail.com>
2020-10-18 11:49:23 +0200

    Add project files.

---
+++ litepadGUI/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace litepadGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WorkArea_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.Modifiers.HasFlag(ModifierKeys.Control) && e.Key == Key.S)
+            {
+                SaveFile();
+            }
+        }
+
+        private void SaveFile()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string lines = WorkArea.Text;
+
+            File.WriteAllText($"{path}/file.txt", lines);
+        }
+    }
+}
